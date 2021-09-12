6b67545e3e056da857b57e17b346d084b8f24ea0
John Esteban Astete Arambarri <john.astete.arambarri@gmail.com>
2021-09-12 03:51:53 -0300

    init testing

---
+++ backend/tests/GetIssueStatusTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Workana\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GetIssueStatusTest extends TestCase
+{
+    public function testIssueIsCreated(): void
+    {
+        $this->assertTrue(true);
+    }
+}
